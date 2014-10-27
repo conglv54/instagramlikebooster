723dfc3eac46194e0a031b6228ec84964803e41f
Lê Công <LeCong@Les-MacBook-Pro.local>
2014-10-27 22:12:03 +0700

    Initial Commit

---
+++ InstagramLikeBooster/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InstagramLikeBooster
+//
+//  Created by Le Cong on 10/27/14.
+//  Copyright (c) 2014 Lê Công. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
